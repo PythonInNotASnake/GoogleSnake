17cdd56d28857c538cbcda3711fc3e73f8e0febc
PythonIsNotASnake <96953402+PythonInNotASnake@users.noreply.github.com>
2022-09-24 15:53:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-document.querySelector("body").style.background = "blue"
+note = document.querySelector("#GInterface\\.Instances\\[2\\]\\.Instances\\[1\\]_1_0_div > div > div > div > div:nth-child(1)")
+note.innerHTML = "20,00"
