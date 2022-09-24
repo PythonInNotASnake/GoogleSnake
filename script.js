c3e542b1911b6cf2a0f09bf10d86e7348751697d
PythonIsNotASnake <96953402+PythonInNotASnake@users.noreply.github.com>
2022-09-24 16:11:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function changeNote(JSpath) {
-  const note = JSpath;
-  note.innerHTML = "20,00"
-}
 
-changeNote(document.querySelector("#GInterface\\.Instances\\[2\\]\\.Instances\\[1\\]_1_1_div > div > div > div > div:nth-child(1) > div.AlignementDroit"));
-changeNote(document.querySelector("#GInterface\\.Instances\\[2\\]\\.Instances\\[1\\]_1_0_div > div > div > div > div:nth-child(1)"));
